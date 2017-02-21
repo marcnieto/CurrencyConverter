f62ac33f4cc8bc23e9bffd8fbce6d1f46d39438e
Marc Nieto <richardhuling@marcbook.local>
2017-02-21 00:54:59 -0800

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Marc Nieto on 2/21/17.
+//  Copyright © 2017 KandidProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
